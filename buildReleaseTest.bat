aaefaae68cc00c00966e3f38b4d8cce3a7cdd905
Mikko <1067314+Tapsa@users.noreply.github.com>
2016-02-15 13:01:45 +0200

    Added more hot keys.

--- buildReleaseTest.bat
+++ buildReleaseTest.bat
@@ -1,5 +1,5 @@
 cd buildR/
-cmake -j4 -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\3wxWidgets" ../
+cmake -j4 -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" ../
 mingw32-make
 strip Kuvastin.exe
 PAUSE
