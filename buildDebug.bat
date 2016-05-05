6097581de0e2789c8dbca521d8e794baadf4a71f
Mikko <mikko.partonen@metropolia.fi>
2016-05-05 13:35:40 +0300

    Filter by size.

--- buildDebug.bat
+++ buildDebug.bat
@@ -1,4 +1,4 @@
 cd buildD/
-cmake -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" ../
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" ../
 mingw32-make -j4
 PAUSE
